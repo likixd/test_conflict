52d508aee1c3177d5495d4acc54f0bfe5fb80cf2
likixd <lici@bk.ru>
2026-09-23 21:48:00 +0400

    3rd commit added 2rd message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main (String[] args) {
         System.out.println("1st message");
         System.out.println("2nd message");
+        System.out.println("3rd message");
     }
 }
