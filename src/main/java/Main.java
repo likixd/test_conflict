2c1fae2aee6bc9f5ebe28f537c79873bd4bfc888
likixd <lici@bk.ru>
2026-09-23 21:59:40 +0400

    5th message +drama

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("1st message");
+        System.out.println("1st message add conflict");
         System.out.println("2nd message");
         System.out.println("3rd message");
         System.out.println("4th message");
+        System.out.println("5th message");
     }
 }
